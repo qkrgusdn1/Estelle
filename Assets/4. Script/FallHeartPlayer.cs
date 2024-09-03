e9d0c2870e942a9e422947c6590eb5bcc3e3c832
qkrgusdn1 <baghyeonu127@gmail.com>
2024-09-03 23:10:13 +0900

    하트 피하고 얻기

---
+++ Assets/4. Script/FallHeartPlayer.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallHeartPlayer : MonoBehaviour
+{
+    public GameObject body;
+
+    public void OnClickedRightBtn()
+    {
+
+    }
+
+    public void OnClickedLeftBtn()
+    {
+
+    }
+}
